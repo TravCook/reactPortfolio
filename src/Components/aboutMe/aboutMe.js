dfc49b7870ba0cfe0db618ebdd058a46eba1443f
Travis Cook <travismcook93@gmail.com>
2020-12-07 18:00:23 -0700

    new layout who dis

--- src/Components/aboutMe/aboutMe.js
+++ src/Components/aboutMe/aboutMe.js
@@ -2,20 +2,147 @@ import React from "react"
 import "./aboutMe.css"
 import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/Button"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
 
 function aboutMe(){
   return(
-  <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title style={{fontSize: '45px'}}>
-      Languages
-    </Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  </Card> 
+  <Row className="container-fluid">
+  <Col>
+    <Card className="centerCard" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title style={{fontSize: '45px'}}>
+          Languages
+        </Card.Title>
+        <Card.Text>
+        <h2 className="listHeader">Front End</h2>
+         <div className="list">
+           <div className = "item">HTML
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+           <div className = "item">CSS
+             <span className="fa fa-star "></span>
+             <span className="fa fa-star "></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+         </div>
+       <h2 className="listHeader">Back End</h2>
+         <div className="list">
+           <div className = "item">JavaScript
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+           <div className = "item">SQL
+             <span className="fa fa-star "></span>
+             <span className="fa fa-star "></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+           <div className = "item">Node.js
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+           <div className = "item">Express
+             <span className="fa fa-star "></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+           <div className = "item">NoSql
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+           <div className = "item">React
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+             <span className="fa fa-star checked"></span>
+           </div>
+         </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </Col>
+  <Col className="abtCol">
+    <Card className="centerCard" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title style={{fontSize: '45px'}}>
+          Technologies
+        </Card.Title>
+        <Card.Text>
+          <div className="technologies">
+            <div className="techItem">Git/Github</div>
+            <div className="techItem">Heroku</div>
+            <div className="techItem">Parralax.js</div>
+            <div className="techItem">Type.it JS</div>
+            <div className="techItem">RIDB API</div>
+            <div className="techItem">Deezer API</div>
+            <div className="techItem">Postman</div>
+            <div className="techItem">Google Geolocation API</div>
+          </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </Col>
+  <Col className="abtCol">
+    <Card className="centerCard" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title style={{fontSize: '45px'}}>
+          Who is Travis Cook?
+        </Card.Title>
+        <Card.Text>
+       <h3>What I enjoy coding</h3>
+         <ul>
+           <li>Back-end functionality</li>
+           <li>React front-ends</li>
+           <li>Any problem that needs solving, it's like a puzzle to me!</li>
+         </ul>
+       <h3>My Free-Time includes</h3>
+         <ul>
+           <li>Family time, and lots of it</li>
+           <li>Side-Projects
+             <ul>
+               <li>Social Media Landing Page Generator</li>
+               <li>Text based adventure webApp</li>
+             </ul>
+           </li>
+           <li>SoloLearn
+             <ul>
+               <li>C++</li>
+               <li>C#</li>
+               <li>React</li>
+             </ul>
+           </li>
+         </ul>
+       <h3>What I Listen to while I Code</h3>
+       <ul>
+         <li>Run the Jewels</li>
+         <li>Heavy Metal</li>
+         <li>NPR</li>
+       </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </Col>
+  </Row>
   )
 }
 export default aboutMe
--- src/Components/contactMe/contactMe.js
+++ src/Components/contactMe/contactMe.js
@@ -1,8 +1,11 @@
 import React from "react"
 import "./contactMe.css"
+import {Document} from "react-pdf"
+import Resume from "../../public/Travis-Cook.pdf"
 
 function contactMe(){
   return(
+    <>
     <div class="jumbotron">
       <h1 class="display-4 Contactheader">Contact Me!</h1>
       <p class="lead">The button below will open up a blank email addressed to me, feel free to use it at any time as I usually check my emails compusively.</p>
@@ -14,6 +17,10 @@ function contactMe(){
       <div class="lead">You just really want/need someone to talk to right now</div>
       <a as="button" class="btn btn-primary btn-lg contactBtn" target="_blank" rel="noreferrer" href="https://mail.google.com/?view=cm&fs=1&to=travismcook93@gmail.com" role="button">Email ME!</a>
     </div>
+    {/* <div className="jumbotron">
+      <Document file={Resume} width="100%" height="100%"></Document>
+    </div> */}
+    </>
   )
 }
 
